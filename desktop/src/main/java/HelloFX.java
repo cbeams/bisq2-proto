edc5bcfc97d45c9d215b3ff643dd44d88e53c9f9
Chris Beams <chris@beams.io>
2021-06-17 09:30:29 +0200

    Bind integer property to label and increment with button

--- desktop/src/main/java/HelloFX.java
+++ desktop/src/main/java/HelloFX.java
@@ -1,17 +1,25 @@
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        IntegerProperty number = new SimpleIntegerProperty(39233);
+        Label label = new Label();
+        label.textProperty().bind(number.asString());
+        Button button = new Button("get price");
+        button.setOnMouseClicked(mouseEvent -> number.set(number.get() + 1));
+        VBox box = new javafx.scene.layout.VBox(label, button);
+        box.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(box, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
