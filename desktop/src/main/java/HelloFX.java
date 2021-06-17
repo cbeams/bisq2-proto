73e29048e69c6ab66af5ff8bb7cc1b315da252b3
Chris Beams <chris@beams.io>
2021-06-17 12:37:20 +0200

    Introduce RxJava 3
    Experiment with binding JavaFX properties to an Rx observable

--- desktop/src/main/java/HelloFX.java
+++ desktop/src/main/java/HelloFX.java
@@ -1,31 +1,60 @@
+import io.reactivex.rxjava3.core.Emitter;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.functions.Consumer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.Random;
 
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        class BisqClient {
+
+            public int getPrice() {
+                return 0;
+            }
+
+            public Observable<Integer> getObservablePrice() {
+                Consumer<Emitter<Integer>> c = emitter -> {
+                    // this is where we would make the REST or WebSocket or gRPC API call.
+                    emitter.onNext(new Random().nextInt());
+                    //
+                    Thread.sleep(1000);
+                };
+                return Observable.generate(c);
+            }
+        }
+
+        BisqClient bisqClient = new BisqClient();
+        int price = bisqClient.getPrice();
+        Observable<Integer> observablePrice = bisqClient.getObservablePrice();
+
+
         IntegerProperty number = new SimpleIntegerProperty(39233);
         Label label = new Label();
         label.textProperty().bind(number.asString());
-        Button button = new Button("get price");
-        button.setOnMouseClicked(mouseEvent -> number.set(number.get() + 1));
-        VBox box = new javafx.scene.layout.VBox(label, button);
+        VBox box = new javafx.scene.layout.VBox(label);
         box.setAlignment(Pos.CENTER);
         Scene scene = new Scene(box, 640, 480);
         stage.setScene(scene);
         stage.show();
+
+        Thread t = new Thread(() -> observablePrice.subscribe(i -> Platform.runLater(() -> number.set(i))));
+        t.setDaemon(true);
+        t.start();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
