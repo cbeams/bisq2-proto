e20e32afa0ea2841d5659474e9a2bf52bb1b033c
Chris Beams <chris@beams.io>
2021-06-19 21:52:17 +0200

    api: Create, show and delete offers
    Fixes: #1

--- http-service/src/main/java/bisq/api/http/service/HttpApiService.java
+++ http-service/src/main/java/bisq/api/http/service/HttpApiService.java
@@ -2,11 +2,20 @@ package bisq.api.http.service;
 
 import bisq.api.client.CoreApiClient;
 import bisq.api.service.ApiService;
+import com.google.gson.Gson;
 import spark.Spark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.*;
 
 public class HttpApiService implements ApiService {
 
-    private CoreApiClient core;
+    private final Gson gson = new Gson();
+    private final List<String> offers = new ArrayList<>();
+
+    private final CoreApiClient core;
     private final int port;
 
     public HttpApiService(CoreApiClient core, int port) {
@@ -18,12 +27,53 @@ public class HttpApiService implements ApiService {
     public void run() {
         Spark.port(port);
         System.out.println("listening on port " + port);
-        Spark.get("/version", (req, res) -> core.getVersion());
-        Spark.get("/price", (req, res) -> core.getPrice());
+
+        get("/version", (req, res) -> core.getVersion());
+
+        get("/price", (req, res) -> core.getPrice());
+
+        get("/offers", (req, res) -> {
+            res.type("application/json");
+            return offers;
+        }, gson::toJson);
+
+        get("/offers/:id", (req, res) -> {
+            res.type("application/json");
+            return offers.get(Integer.parseInt(req.params("id")) - 1);
+        }, gson::toJson);
+
+        post("/offers", (req, res) -> {
+            var offer = gson.fromJson(req.body(), String.class);
+            offers.add(offer);
+            res.status(201);
+            res.header("Location", "/offers/" + (offers.lastIndexOf(offer) + 1));
+
+            res.type("application/json");
+            return gson.toJson(offer);
+        });
+
+        delete("/offers", (req, res) -> {
+            offers.clear();
+            res.status(204);
+            res.type("application/json");
+            return "";
+        });
+
+        delete("/offers/:id", (req, res) -> {
+            res.type("application/json");
+            offers.remove(Integer.parseInt(req.params("id")) - 1);
+            res.status(204);
+            res.type("application/json");
+            return "";
+        }, gson::toJson);
     }
 
     @Override
     public void stop() {
         Spark.stop();
+    }
+
+    public static void main(String[] args) {
+        new HttpApiService(new CoreApiClient(), 9999).run();
     }
 }
